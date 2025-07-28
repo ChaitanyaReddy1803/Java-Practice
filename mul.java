7d1c5f33c8444cfb895209f41a85a5b69eeb22ee
Chaitanya1803 <pranawreddy1803@gmail.com>
2025-07-28 23:22:58 +0530

    Update program

---
+++ mul.java
@@ -0,0 +1,10 @@
+public class mul {
+    public static void main(String[] args) {
+        int num1 = 5; // First number
+        int num2 = 4; // Second number
+
+        int result = num1 * num2; // Multiplication
+
+        System.out.println("Multiplication of " + num1 + " and " + num2 + " is: " + result);
+    }
+}
