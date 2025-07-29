d98d2b5526b306be6034ac1a24f17ef883bb4782
Chaitanya1803 <pranawreddy1803@gmail.com>
2025-07-29 21:56:28 +0530

    Subtraction program

---
+++ Subtraction.java
@@ -0,0 +1,11 @@
+public class Subtraction {
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 7;
+        int result = a - b;
+
+        System.out.println("First Number: " + a);
+        System.out.println("Second Number: " + b);
+        System.out.println("Subtraction Result: " + result);
+    }
+}
--- mul.java
+++ mul.java
@@ -1,9 +1,9 @@
 public class mul {
     public static void main(String[] args) {
-        int num1 = 5; // First number
-        int num2 = 4; // Second number
+        int num1 = 5; 
+        int num2 = 4;
 
-        int result = num1 * num2; // Multiplication
+        int result = num1 * num2; 
 
         System.out.println("Multiplication of " + num1 + " and " + num2 + " is: " + result);
     }
