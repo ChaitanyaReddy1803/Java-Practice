f329ef7d6d3b27da09a59070f2289eefb07764c4
Chaitanya1803 <pranawreddy1803@gmail.com>
2025-07-31 00:22:09 +0530

    Add EvenOrOdd program

---
+++ EvenOrOdd.java
@@ -0,0 +1,11 @@
+public class EvenOrOdd {
+    public static void main(String[] args) {
+        int number = 7; // You can change this number to test
+
+        if (number % 2 == 0) {
+            System.out.println(number + " is Even");
+        } else {
+            System.out.println(number + " is Odd");
+        }
+    }
+}
