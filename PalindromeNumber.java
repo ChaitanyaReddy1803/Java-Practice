ccbfc27cac42fc10fcc049d5b20ef1c502ca4da4
Chaitanya1803 <pranawreddy1803@gmail.com>
2025-08-04 21:35:27 +0530

    Added palindrome number checker

---
+++ PalindromeNumber.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class PalindromeNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int originalNumber = scanner.nextInt();
+        int number = originalNumber;
+        int reversed = 0;
+
+        while (number != 0) {
+            int digit = number % 10;
+            reversed = reversed * 10 + digit;
+            number /= 10;
+        }
+
+        if (originalNumber == reversed) {
+            System.out.println(originalNumber + " is a palindrome number.");
+        } else {
+            System.out.println(originalNumber + " is not a palindrome number.");
+        }
+
+        scanner.close();
+    }
+}
