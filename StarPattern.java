0aaf252f3191213216ed994a4bbfc44f6960120e
Chaitanya1803 <pranawreddy1803@gmail.com>
2025-08-11 22:18:48 +0530

    Added star pyramid pattern program

---
+++ StarPattern.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class StarPattern {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int rows = scanner.nextInt();
+
+        for (int i = 1; i <= rows; i++) {
+            // Print spaces
+            for (int space = 1; space <= rows - i; space++) {
+                System.out.print(" ");
+            }
+            // Print stars
+            for (int star = 1; star <= (2 * i - 1); star++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        scanner.close();
+    }
+}
