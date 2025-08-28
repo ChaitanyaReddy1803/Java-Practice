4fb3ab9aa65ef92887725482dd7c68e0d2ddc0b4
Chaitanya1803 <pranawreddy1803@gmail.com>
2025-08-28 12:04:56 +0530

    Step 5: Added merge logic for first array

--- MergeArrays.java
+++ MergeArrays.java
@@ -2,26 +2,27 @@ import java.util.Scanner;
 
 public class MergeArrays {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-        // Input first array
         System.out.print("Enter size of first array: ");
-        int n1 = sc.nextInt();
+        int n1 = scanner.nextInt();
         int[] arr1 = new int[n1];
-        System.out.println("Enter elements of first array:");
+        System.out.println("Enter " + n1 + " elements for first array:");
         for (int i = 0; i < n1; i++) {
-            arr1[i] = sc.nextInt();
+            arr1[i] = scanner.nextInt();
         }
 
-        // ✅ Input second array (Step 4)
         System.out.print("Enter size of second array: ");
-        int n2 = sc.nextInt();
+        int n2 = scanner.nextInt();
         int[] arr2 = new int[n2];
-        System.out.println("Enter elements of second array:");
+        System.out.println("Enter " + n2 + " elements for second array:");
         for (int i = 0; i < n2; i++) {
-            arr2[i] = sc.nextInt();
+            arr2[i] = scanner.nextInt();
         }
 
-        sc.close();
+        int[] merged = new int[n1 + n2];
+        for (int i = 0; i < n1; i++) {
+            merged[i] = arr1[i];
+        }
     }
 }
