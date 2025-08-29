081f9f59102e905a604f99299fe8dc067786b05d
ChaitanyaReddy1803 <pranawreddy1803@gmail.com>
2025-08-29 23:49:12 +0530

    Update mul.java

--- mul.java
+++ mul.java
@@ -1,7 +1,7 @@
 public class mul {
     public static void main(String[] args) {
-        int num1 = 5; 
-        int num2 = 4;
+        int num1 = 5+5; 
+        int num2 = 4+4;
 
         int result = num1 * num2; 
 
